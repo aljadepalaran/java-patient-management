c613b0dd82c0141fc27f89e5ae00d39bc5a32257
Al Jade <aljade1902@gmail.com>
2019-12-20 23:31:13 +0000

    Class creation.

---
+++ coursework/src/coursework/Users/Doctor.java
@@ -0,0 +1,13 @@
+package coursework.Users;
+
+public class Doctor extends User{
+    
+    public Doctor(String _uniqueID, String _password, String _firstName, 
+            String _lastName, char _accessKey){
+        
+        super(_uniqueID, _password, _firstName, _lastName, _accessKey); //constructs from base class
+        
+        this.accessKey = 'D';
+        
+    }
+}
---
+++ coursework/src/coursework/Users/User.java
@@ -0,0 +1,22 @@
+package coursework.Users;
+
+public class User {
+    
+    String uniqueID;
+    String password;
+    String firstName;
+    String lastName;
+    char accessKey;
+    
+    public User(String _uniqueID, String _password, String _firstName, 
+            String _lastName, char _accessKey){
+        
+        this.uniqueID = _uniqueID;
+        this.password = _password; 
+        this.firstName = _firstName;
+        this.lastName = _lastName;
+        this.accessKey = _accessKey;
+        
+    }
+    
+}
