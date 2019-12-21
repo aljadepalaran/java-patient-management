0290d9b09eae82de68bae78f489540d2eca5d75b
Al Jade <aljade1902@gmail.com>
2019-12-21 17:04:17 +0000

    Functions for authentication added. Data file created to store the credentials.

--- coursework/src/Default/Functions.java
+++ coursework/src/Default/Functions.java
@@ -1,32 +1,24 @@
 package Default;
+import Java.IO.*;
 
 public class Functions {
-    
     public static void ParseCSV(){
-        //parse file and convert to classes
+        filePath = "";
+        FileReader parser = FileReader(filePath);
     }
-    
     public static String convertCharToString(char[] _array){
-            
         String output = "";
         char[] currentArray = _array;
         int arrayLength = currentArray.length;
-        
         for(int x = 0; x < arrayLength; x++){
             output = output + currentArray[x];
         }
-        
         return output;
-        
     }
-    
     public static boolean authenticateLogin(String _username, String _password){
-        
         boolean authFlag = false; //default to false
         String username = _username;
         String password = _password;
-        
-        
         if(username.compareTo("aljadepalaran") == 0){
             if(password.compareTo("test") == 0){
                 authFlag = true;
@@ -38,16 +30,10 @@ public class Functions {
         else{
             //auth failed
         }
-        
         return authFlag; 
-        
     }
-    
     public static String getPasswordFromFile(){
         String output = "";
-        
-        
         return output;
     }
-    
 }
