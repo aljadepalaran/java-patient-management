ccbd113c1c5b56cbe8ed45ef09d7cec49ced65d5
Al Jade <aljade1902@gmail.com>
2020-01-02 19:22:43 +0000

    Created a new class to store populate methods.

---
+++ coursework/src/coursework/Functions/Populate.java
@@ -0,0 +1,32 @@
+package coursework.Functions;
+
+public class Populate {
+    
+    public static void populateFile(){
+        populateUsers();
+        populateAppointments();
+        populateFeedbacks();
+        populateMedicines();
+        populatePrescriptions();
+        populateRatings();
+    }
+    
+    public static void populateUsers(){
+        
+    }
+    public static void populateAppointments(){
+        
+    }
+    public static void populateFeedbacks(){
+        
+    }
+    public static void populateMedicines(){
+        
+    }
+    public static void populatePrescriptions(){
+        
+    }
+    public static void populateRatings(){
+        
+    }
+}
